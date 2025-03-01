173cc2848f1e8b0d58b9e935328c3896310afd4d
Yusuf91132 <sabinader6@gmail.com>
2025-03-01 16:45:59 +0600

    fourth commit

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,5 +1,129 @@
 from django.db import models
 
 # Create your models here.
-class Test(models.Model):
-    name = models.TextField()
+class Info(models.Model):
+    name = models.CharField(
+        max_length=55,
+        verbose_name="Имя Компании"
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телофонный Номер"
+    )
+    vertical_text = models.CharField(
+        max_length=100,
+        verbose_name="Вертикальный Текст"
+    )
+    class Meta:
+        verbose_name = "Информация"
+        verbose_name_plural = "Информации"
+    def __str__(self):
+        return self.name
+
+class Navigation(models.Model):
+    home = models.CharField(
+        max_length=55,
+        verbose_name="1 Навигационная Кнопка"
+    ) 
+    about = models.CharField(
+        max_length=55,
+        verbose_name="2 Навигационная Кнопка"
+    )
+    projects = models.CharField(
+        max_length=55,
+        verbose_name="3 Навигационная Кнопка"
+    )
+    services = models.CharField(
+        max_length=55,
+        verbose_name="4 Навигационная Кнопка"
+    )
+    contacts = models.CharField(
+        max_length=55,
+        verbose_name="5 Навигационная Кнопка"
+    )
+    previous_button = models.CharField(
+        max_length=55,
+        verbose_name="Предыдущая Кнопка"
+    )
+    next_button = models.CharField(
+        max_length=55,
+        verbose_name="Следующая Кнопка"
+    )
+    look_more_button = models.CharField(
+        max_length=35,
+        verbose_name="Кнопка Смотреть Дальше"
+    ) 
+    def __str__(self):
+        return self.home
+    
+    class Meta:
+        verbose_name = "Навигация"
+        verbose_name_plural = "Навигации"
+
+class MainProjects(models.Model):
+    image = models.ImageField(
+        verbose_name="Фото Проэкта"
+    )
+    project_name = models.CharField(
+        max_length=100,
+        verbose_name="Имя Проэкта"
+    )
+    project_desription = models.TextField(
+        verbose_name="Описание Проэкта"
+    )
+    def __str__(self):
+        return self.project_name
+    class Meta:
+        verbose_name = "Главный Проэкт"
+        verbose_name = "Главные Проэкты"
+
+class About(models.Model):
+    title = models.CharField(
+        max_length=155,
+        verbose_name="Заголовок"
+    )
+    citate = models.CharField(
+        max_length=155,
+        verbose_name="Цитата"
+    )
+    description = models.TextField(
+        verbose_name="Описания О Нас"
+    )
+    specialization_title = models.CharField(
+        max_length=55,
+        verbose_name="Наши Специальности"
+    )
+    specialization = models.CharField(
+        max_length=55,
+        verbose_name="Специальности"
+    )
+    logo = models.ImageField(
+        verbose_name="Лого"
+    )
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = "О нас"
+
+class OurProjects(models.Model):
+    title = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name="Заголовок"
+    )
+    image = models.ImageField(
+        verbose_name="Фото Проэкта"
+    ) 
+    project_name = models.CharField(
+        max_length=100,
+        verbose_name="Названия Проэкта"
+    )
+    vertical_line = models.CharField(
+        max_length=55,
+        verbose_name="Вертикальная Линия"
+    )      
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = "Наши Проэкты"

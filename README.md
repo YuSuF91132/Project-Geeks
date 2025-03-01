d80ccc83d0451418803433f1f67bab422cc4409e
Yusuf91132 <sabinader6@gmail.com>
2025-03-01 11:22:31 +0600

    third commit

--- README.md
+++ README.md
@@ -2,18 +2,32 @@
 Веб-приложения для geeks.com
 
 Teamlead:Yusuf
+
 Design:Yusuf
+
 Frontend:Yusuf
+
 Backend:Yusuf
+
 Ideas:Yusuf
 
 version 1.0
     Был создан сам проэкт.
+
     Подключил БД PostgreSQL.
+
     Настроил settings.py.
+
     Настроил static и media.
 
 version 1.1
     Была добавлена домашняя страница
+
+version 1.2
+    Польностью новый дизайн
+
+    Решили проблему с статическими файлами
+
+
 
 
--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import HomePageView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name= 'home_page')
+    path('', HomePageView.as_view(), name= 'home')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
